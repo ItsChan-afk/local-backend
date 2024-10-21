e92cbd9cac862e89ce9f57cbb18ae468e42e6aa2
Mac <chanminswe26@gmail.com>
2024-10-21 16:40:39 +0630

    cors fixing 1

--- controllers/authentication/login.js
+++ controllers/authentication/login.js
@@ -32,13 +32,12 @@ const login = async (req, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "None",
       });
-      return res
-        .status(200)
-        .json({
-          message: "Login Successful",
-          user: { id: user._id, username: user.username },
-        });
+      return res.status(200).json({
+        message: "Login Successful",
+        user: { id: user._id, username: user.username },
+      });
     }
   } catch (error) {
     console.log("Error Occured at Login", error);
